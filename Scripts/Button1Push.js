ad7755bead4d536731486c932e12778ea0d8c004
Samuel Eleftheri <samueleleftheri@gmail.com>
2016-05-10 10:52:52 -0500

    Cleaner Comments

--- Scripts/Button1Push.js
+++ Scripts/Button1Push.js
@@ -1,9 +1,10 @@
 ﻿#pragma strict
 
-// Activates existing gameObjects that were not active.
+// Activates existing gameObjects that were not active. Note that the gameObjects were never SetActive(true) at start, 
+// so make sure to have your gameObjects set to false (not activated/displayed)
 
-public var button1GameObject : GameObject; // gameObject activated from clicking button1
-private var buttonActivate1 : boolean = false; // determines whether button1 is activated
+public var button1GameObject : GameObject; // gameObject activated from activating button1, assign what gameObjects you want to setActive.
+private var buttonActivate1 : boolean = false; // determines whether button1 (we created a gameObject that acted like a button) is activated
 
 function Start () {
 
@@ -15,16 +16,16 @@ function Update () {
 
 function buttonOnOff() {
 	if (buttonActivate1 == false) {
-		Debug.Log("You turned on button1");
-		buttonActivate1 = true;
-		gameObject.GetComponent.<Renderer>().material.color = Color.green;
-		button1GameObject.SetActive(true);
+		Debug.Log("You turned on button1"); //debugging log
+		buttonActivate1 = true; // // button is activated (set true)
+		gameObject.GetComponent.<Renderer>().material.color = Color.green; // button turned green for visual confirmation
+		button1GameObject.SetActive(true); // gameObjects now visable
 	}
 	else {
-		Debug.Log("You turned off button1");
-		buttonActivate1 = false;
-		gameObject.GetComponent.<Renderer>().material.color = Color.red;
-		button1GameObject.SetActive(false);
+		Debug.Log("You turned off button1"); // debugging log
+		buttonActivate1 = false; // button is not active
+		gameObject.GetComponent.<Renderer>().material.color = Color.red; // button turns red for visual confirmation
+		button1GameObject.SetActive(false); // gameObjects not visable anymore
 	}
 }
 
