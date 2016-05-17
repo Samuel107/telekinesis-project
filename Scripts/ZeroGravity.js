3c06d7c4686a2198ae48ba0d6b746509dfb9c698
Samuel Eleftheri <samueleleftheri@gmail.com>
2016-05-17 14:50:35 -0500

    Updated comments

--- Scripts/ZeroGravity.js
+++ Scripts/ZeroGravity.js
@@ -1,9 +1,12 @@
 ﻿#pragma strict
 
-// This Script (Button 3) Activates Zero-Gravity On Certain GameObjects with tag "abc".
+// This Script Activates Zero-Gravity On Certain GameObjects with tag "abc".
 
-private var buttonActivate2 : boolean = false; // determines whether button1 is activated with bool
+//public
 public var rotationSpeed : int = 5; // rotation speed
+
+//private
+private var buttonActivate2 : boolean = false; // determining variable that activates buttonOnOff function
 
 function Start () {
 
@@ -15,10 +18,11 @@ function Update () {
 
 function buttonOnOff() {
 	if (buttonActivate2 == false) { // if button is not on
-		Debug.Log("You turned on button2"); // debug purpose
+		//Debug.Log("You turned on button2"); 
 		buttonActivate2 = true; // button set true
 		gameObject.GetComponent.<Renderer>().material.color = Color.green; // button turned green for visual confirmation
 		gameObject.GetComponent.<AudioSource>().Play(); // plays background music (comment out if you include no background music)
+		
 		for (var go : GameObject in GameObject.FindGameObjectsWithTag("abc")) { // looks for every gameObject with tag "abc"
 			var leftOrRight : int = Random.Range(1,3); // creates random integer that is either 1 or 2. (decision integer)
 			var addedForce : int = Random.Range(10,50); // creates random integer 10-50 (force integer)
@@ -34,12 +38,12 @@ function buttonOnOff() {
 		}
 	}
 	else { 
-		Debug.Log("You turned off button2"); // debug purpose
+		//Debug.Log("You turned off button2"); 
 		buttonActivate2 = false; // button set false
 		gameObject.GetComponent.<Renderer>().material.color = Color.red; // button turned red for visual confirmation
 		gameObject.GetComponent.<AudioSource>().Stop(); // stops audio (comment out if no audio put in)
 		for (var go : GameObject in GameObject.FindGameObjectsWithTag("abc")) { // every gameObject with tag "abc" 
-			go.GetComponent.<Rigidbody>().useGravity = true; // turns gravity back on for go
+			go.GetComponent.<Rigidbody>().useGravity = true; // turns gravity back on for every go
 		}
 	}
 }
