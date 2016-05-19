375ebb5e1da5475bdc43543620297368c61b133f
Samuel Eleftheri <samueleleftheri@gmail.com>
2016-05-19 13:11:48 -0500

    Cleaner Comments

--- Scripts/ActivateGameObjects/Activate-GameObjects.js
+++ Scripts/ActivateGameObjects/Activate-GameObjects.js
@@ -21,13 +21,13 @@ function Update () {
 
 function buttonOnOff() {
 	if (buttonActivate1 == false) {
-		Debug.Log("You turned on button1"); //debugging log
+		// Debug.Log("You turned on button1"); 
 		buttonActivate1 = true; // // button is activated (set true)
 		gameObject.GetComponent.<Renderer>().material.color = Color.green; // button turned green for visual confirmation
 		button1GameObject.SetActive(true); // gameObjects now visable
 	}
 	else {
-		Debug.Log("You turned off button1"); // debugging log
+		// Debug.Log("You turned off button1"); 
 		buttonActivate1 = false; // button is not active
 		gameObject.GetComponent.<Renderer>().material.color = Color.red; // button turns red for visual confirmation
 		button1GameObject.SetActive(false); // gameObjects not visable anymore
