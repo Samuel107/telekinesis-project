8e7a09284aa1f144d8c1c7e8aef63974fcfbebf5
Samuel Eleftheri <samueleleftheri@gmail.com>
2016-05-20 12:33:50 -0500

    Cleaner Comments

--- Scripts/RestartsScene/RestartsScene.js
+++ Scripts/RestartsScene/RestartsScene.js
@@ -1,6 +1,11 @@
 ﻿#pragma strict
 
-// Restarts Scene
+/*
+	Restarts Current Scene:
+		This Script Is Actualy Very Wrong In Someway, But It Reloads Current Scene Which Is What I Wanted It To Do.
+		Probably has something to do with the SceneManager Variable. It is NULL now that I realize it.
+		Look at B6SendToPlanets.cs script if you want to learn to load other scenes properly.
+*/
 
 var ls : UnityEngine.SceneManagement.SceneManager; // created variable with SceneManagement Options
 
@@ -13,5 +18,5 @@ function Update () {
 }
 
 function resetTheScene() {
-	ls.LoadScene("newRoom"); // restarts scene ("newRoom" is my scene name)
+	ls.LoadScene("newRoom"); // restarts scene ("Room" is my scene name)
 }
